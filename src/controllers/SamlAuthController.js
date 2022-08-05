3adce29ac2508690a01b56b88c8264477da5961e
lavender <98966411+lavenderMLE@users.noreply.github.com>
2022-08-05 22:50:52 +0300

    added API

--- src/controllers/SamlAuthController.js
+++ src/controllers/SamlAuthController.js
@@ -3,9 +3,10 @@
 const axios = require('axios') ;
 const { errorMonitor } = require('multer-gridfs-storage');
 
-// const AppError = require('../utils/appError') ;
+const AppError = require('../utils/appError') ;
 
 const saml = require('samlify');
+
 
 exports.getMetaDataInfo = async (req, res, next) => {
       
@@ -13,47 +14,40 @@ exports.getMetaDataInfo = async (req, res, next) => {
     const uri_forti_metadata = 'https://app.onelogin.com/saml/metadata/487043' ;
 
     
-    axios.get(uri_forti_metadata).then(function(data){
-        res.status(200).json({ data : data});
-    })
-    .catch(function(error){
-        res.status(500).send(error) ;
-    })    
+    // axios.get(uri_forti_metadata).then(function(data){
+    //     res.status(200).json({ data : data});
+    // })
+    // .catch(function(error){
+    //     res.status(500).send(error) ;
+    // })    
         
     
-    // try {
-    //     let response = await axios.get( uri_forti_metadata ) ;
-        
-    //     return res.status(200).json({
-    //         ...response
-    //     })
-    // }catch(err){
-    //     return res.status(200).json({
-    //         ...err
-    //     }) ;
-    // }   
-
-    //     if (response.data ) {                    
-    //         return next(new AppError(200, 'getAllRecipient', 'recipients does not exist'));
-    //     } else {
-    //         return next(new AppError(403, 'getAllRecipient', 'recipients does not exist'));
-    //     }
+    try {
+        let response = await axios.get( uri_forti_metadata ) ;
+                    
+        if (response.data ) { 
+            return next(new AppError(200, 'getAllRecipient', response.data ));
+        } else {
+            return next(new AppError(403, 'getAllRecipient', response.data ));
+        }
         
         
-    //     idp = saml.IdentityProvider({
-    //         metadata: response.data,
-    //         isAssertionEncrypted: true,
-    //         messageSigningOrder: 'encrypt-then-sign',
-    //         wantLogoutRequestSigned: true
-    //         }) ;            
+        // idp = saml.IdentityProvider({
+        //     metadata: response.data,
+        //     isAssertionEncrypted: true,
+        //     messageSigningOrder: 'encrypt-then-sign',
+        //     wantLogoutRequestSigned: true
+        //     }) ;            
             
-    //         // console.log( idp ) ;
-    //         // const sp = saml.ServiceProvider({
-    //         //   entityID: 'http://localhost:3000/sso/metadata',
-    //         // })
-    // } catch(err) {
-    //     console.log(err) ;
-    // }           
+            // console.log( idp ) ;
+            // const sp = saml.ServiceProvider({
+            //   entityID: 'http://localhost:3000/sso/metadata',
+            // })
+    } catch(err) {
+        return res.status(500).json({
+            status: 'failed',
+        })
+    }           
     
     // return res.status(200).json({
     //     status : "success",
